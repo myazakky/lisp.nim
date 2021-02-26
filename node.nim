2059e630554922bcb01895ffe795e37d0d627cf3
nimuC <nimuC@outlook.com>
2021-02-26 17:21:36 +0900

    NumberLiteralの表示をよくした
    分母が1の時は分母を非表示

--- node.nim
+++ node.nim
@@ -46,7 +46,9 @@ func `$`*(node: LispNode): string =
     case node.kind
     of IdentifierLiteral: fmt"<Identifer: {node.identifierValue}>"
     of SymbolLiteral: fmt"'{node.symbolValue}"
-    of NumberLiteral: fmt"{node.topValue}/{node.bottomValue}"
+    of NumberLiteral:
+      if node.bottomValue == 1: $node.topValue
+      else: fmt"{node.topValue}/{node.bottomValue}"
     of BooleanLiteral: fmt"#{$node.booleanValue}"
     of NilLiteral: "'()"
     of PairLiteral: fmt"'({$node.car} {$node.cdr})"
